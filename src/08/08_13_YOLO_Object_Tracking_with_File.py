6fa11aaefddafaa5aa98696d276291450a94f199
Happy Coder <mc6666@gmail.com>
2025-03-12 11:44:10 +0800

    Add files via upload

---
+++ src/08/08_13_YOLO_Object_Tracking_with_File.py
@@ -0,0 +1,24 @@
+import cv2
+from ultralytics import YOLO
+
+# Load the YOLO11 model
+model = YOLO("yolo11n.pt")
+
+# Open the video file
+video_path = "./walk.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO11 Tracking", annotated_frame)
